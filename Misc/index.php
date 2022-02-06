ad13c9543fd6a2219f3ea9d51232223d7aa0e761
grantgariepy <grantgariepy@protonmail.com>
2022-02-05 18:53:40 -0700

    update

--- Misc/index.php
+++ Misc/index.php
@@ -1,25 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Misc</title>
-    <link rel="stylesheet" type="text/css" href="../style.css" />
+    <link rel="stylesheet" type="text/css" href="/style.css" />
 </head>
+
 <body>
-    
-<?php
+    <div>
+        <p>
+            <?php
 
 
-    $array = array("Grant", "Jon", "Arya", "Sansa", "Sam");
+            $array = array("Grant", "Jon", "Arya", "Sansa", "Sam");
 
-    foreach ($array as $loopdata) {
-        echo "My name is ".$loopdata."<br>";
+            foreach ($array as $loopdata) {
+                echo "My name is " . $loopdata . "<br>";
+            }
 
-    }
-
-?>
-
+            ?>
+        </p>
+    </div>
 </body>
+
 </html>
